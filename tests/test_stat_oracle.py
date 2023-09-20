7745f8893f7cc5e2f03434a054a4811f5b05b20a
Jack Stevenson <jack.stevenson9001@gmail.com>
2023-09-20 21:45:49 +0100

    Added tensorflow for gh-actions testing

--- tests/test_stat_oracle.py
+++ tests/test_stat_oracle.py
@@ -2,9 +2,11 @@ from keras_stat_tuner import StatisticalOracle, StatisticalSearch
 from keras_stat_tuner.StatsTuner import ImplementationError
 import pytest
 
+
 def test_raise_error_if_no_estimator_oracle():
     with pytest.raises(ImplementationError):
         StatisticalOracle(estimator=None)
+
 
 def test_raise_error_if_no_estimator_tuner():
     with pytest.raises(ImplementationError):
